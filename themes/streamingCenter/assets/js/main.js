c56aea881177ab44b2cb093db8e38a850e668a59
Alexey V Paramonov <me@paramonov.ru>
2024-10-02 08:05:57 +0300

    Try before buy script update

--- themes/streamingCenter/assets/js/main.js
+++ themes/streamingCenter/assets/js/main.js
@@ -6,13 +6,24 @@ const validateEmail = (email) => {
     );
   };
 
-const restoreButton = function(){
+
+function restoreButton(){
     const tryBtn = document.getElementById("try-btn");
     tryBtn.classList.remove('button-error');
     tryBtn.innerHTML = originalButtonText;
     tryBtn.removeAttribute("disabled");
 }
 
+function addErrorToButton(text){
+    const tryForm = document.getElementById("try-form");
+    const tryBtn = document.getElementById("try-btn");
+
+    tryForm.classList.add('form-error');
+    tryBtn.classList.add('button-error');
+    tryBtn.innerHTML = text;
+    tryBtn.setAttribute("disabled", "disabled");
+    setTimeout(restoreButton, 2000)
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     
@@ -22,27 +33,55 @@ document.addEventListener('DOMContentLoaded', function() {
 
     originalButtonText = tryBtn.innerHTML;
     const invalidEmailText = tryBtn.getAttribute('data-invalid-email');
+    const langCode = tryBtn.getAttribute('data-lang');
+    const trySuccessText = tryBtn.getAttribute('data-success-text');
+    const tryContainer = document.getElementById("try-container");
 
     tryBtn.addEventListener('click', async function(event){
         event.preventDefault();
         tryForm.classList.remove('form-error');
         restoreButton();
         if(!tryEmail.value || !validateEmail(tryEmail.value)){
-            tryForm.classList.add('form-error');
-            tryBtn.classList.add('button-error');
-            tryBtn.innerHTML = invalidEmailText;
-            tryBtn.setAttribute("disabled", "disabled");
-            setTimeout(restoreButton, 2000)
+            addErrorToButton(invalidEmailText);
             return;
         }
-        const response = await fetch("https://streaming.center/api/try_before_buy", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email: tryEmail.value})
-        });
+
+        tryBtn.setAttribute("disabled", "disabled");
+
+        try {
+            const response = await fetch("https://streaming.center/api/v1/users/", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email: tryEmail.value, language: langCode == 'en' ? 0 : 1, currency: langCode == 'en' ? 0 : 1})
+            });
+            if(!response.ok){
+                // const data = await response.json();
+                // const error_code = data['email'][0];
+                // tryBtn.removeAttribute("disabled");
+                addErrorToButton(invalidEmailText);
+                //let errorText = invalidEmailText;
+                // switch(error_code){
+                //     case 'unique':
+                //     // case 'email':
+                // }
+                return;
+            }
+            tryBtn.removeAttribute("disabled");
+
+            // All good: show greeting
+            tryContainer.innerHTML = `${trySuccessText} <b>${tryEmail.value}<b>`;
+            tryContainer.classList.add('area-success');
+                
+        }
+        catch (err) {
+            console.log("Error: ", err)
+            tryBtn.removeAttribute("disabled");
+
+        }
+
           
     });
 
