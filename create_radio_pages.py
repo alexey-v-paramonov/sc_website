c5795fc2f4a4bbdf2219cdfcf34d818dc80cb3e1
Alexey V Paramonov <me@paramonov.ru>
2025-09-19 22:59:45 +0300

    Add flags

--- create_radio_pages.py
+++ create_radio_pages.py
@@ -16,11 +16,15 @@ def create_radio_pages():
         # English page
         en_path = f"content/en/catalog/{slug}.md"
         os.makedirs(os.path.dirname(en_path), exist_ok=True)
+        country = radio.get("country", {})
+        country_code = country.get("iso2", None).lower()
         with open(en_path, 'w') as f:
             f.write('---\n')
             title = radio.get("name", "").replace("\"", "'")
             f.write(f'title: "{title}"\n')
             f.write(f'type: "catalog_item"\n')
+            if country_code:
+                f.write(f'country_code: "{country_code}"\n')
             f.write(f'logo: "{radio.get("logo", "")}"\n')
             f.write(f'website_url: "{radio.get("website_url", "")}"\n')
             description = radio.get("description", "").replace("\"", "'")
@@ -35,6 +39,9 @@ def create_radio_pages():
             title = radio.get("name", "").replace("\"", "'")
             f.write(f'title: "{title}"\n')
             f.write(f'type: "catalog_item"\n')
+            if country_code:
+                f.write(f'country_code: "{country_code}"\n')
+
             f.write(f'logo: "{radio.get("logo", "")}"\n')
             f.write(f'website_url: "{radio.get("website_url", "")}"\n')
             description = radio.get("description", "").replace("\"", "'")
